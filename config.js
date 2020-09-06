6549782b05405763447a652819e8672b83628963
Danish Jamal <danishjamal.104@gmail.com>
2020-09-07 01:43:07 +0530

    outh 2.0 configured and implemented

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '936644330157141',
+        clientSecret: '2d51f262e21e2dd11235782be8d51ec9'
+    }
 }
